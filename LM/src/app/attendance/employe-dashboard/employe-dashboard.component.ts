d4b001ed727d185697c0e1e4eb413a771dea79c9
bknasam <bknasam@sreebtech.com>
2022-06-16 17:58:52 +0530

    update the dash board design

--- LM/src/app/attendance/employe-dashboard/employe-dashboard.component.ts
+++ LM/src/app/attendance/employe-dashboard/employe-dashboard.component.ts
@@ -1,24 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
-
+import { CalendarOptions, EventInput } from '@fullcalendar/angular'; // useful for typechecking
+import { MatDividerModule } from '@angular/material/divider';
 @Component({
   selector: 'app-employe-dashboard',
   templateUrl: './employe-dashboard.component.html',
   styleUrls: ['./employe-dashboard.component.scss']
 })
 export class EmployeDashboardComponent implements OnInit {
-
+  // calendarOptions: CalendarOptions = {
+  //   initialView: 'dayGridMonth',
+  // //  dateClick: this.handleDateClick.bind(this), // bind is important!
+  //   events: [
+  //     {
+  //       title: 'Present',
+  //       start: '2020-06-12T10:30:00',
+  //       end: '2020-06-12T11:00:00',
+  //     },
+  //     { title: 'event 1', date: '2020-06-15' },
+  //     { title: 'event 2', date: '2020-06-30' }
+  //   ]
+  // };
+  // handleDateClick(arg:any) {
+  //   alert('date click! ' + arg.dateStr)
+  // }
+   TODAY_STR = new Date().toISOString().replace(/T.*$/, '');
+  initialEvents:EventInput[] = [
+    
+      {
+        title: 'Present',
+        start: '2020-06-12T10:30:00',
+        
+      },
+      {
+        title: 'Present',
+        start: '2020-06-13T10:30:00',
+        
+      },
+      {
+        title: 'Present',
+        start: '2020-06-15T10:30:00',
+        
+      },
+    
+  ];
   calendarOptions: CalendarOptions = {
+    headerToolbar: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'dayGridMonth'
+    },
     initialView: 'dayGridMonth',
-    dateClick: this.handleDateClick.bind(this), // bind is important!
-    events: [
-      { title: 'event 1', date: '2020-06-27' },
-      { title: 'event 2', date: '2020-06-30' }
-    ]
+    initialDate: '2020-06-07',
+   initialEvents: this.initialEvents, // alternatively, use the `events` setting to fetch from a feed
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    eventColor: '#378006'
+
+    // select: this.handleDateSelect.bind(this),
+    // eventClick: this.handleEventClick.bind(this),
+    // eventsSet: this.handleEvents.bind(this)
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
   };
-  handleDateClick(arg:any) {
-    alert('date click! ' + arg.dateStr)
-  }
+
+
   constructor() { }
 
   ngOnInit(): void {
--- LM/src/app/attendance/manager-dashboard/manager-dashboard.component.ts
+++ LM/src/app/attendance/manager-dashboard/manager-dashboard.component.ts
@@ -1,28 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
 
+import { CalendarOptions, EventInput } from '@fullcalendar/angular'; // useful for typechecking
+import { FullCalendarModule } from '@fullcalendar/angular'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
 @Component({
   selector: 'app-manager-dashboard',
   templateUrl: './manager-dashboard.component.html',
   styleUrls: ['./manager-dashboard.component.scss']
 })
 export class ManagerDashboardComponent implements OnInit {
-
+  TODAY_STR = new Date().toISOString().replace(/T.*$/, '');
+  initialEvents:EventInput[] = [
+    
+      {
+        title: 'Present',
+        start: '2020-06-12T10:30:00',
+        
+      },
+      {
+        title: 'Present',
+        start: '2020-06-13T10:30:00',
+        
+      },
+      {
+        title: 'Present',
+        start: '2020-06-15T10:30:00',
+        
+      },
+    
+  ];
   calendarOptions: CalendarOptions = {
+    headerToolbar: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'dayGridMonth'
+    },
     initialView: 'dayGridMonth',
-    dateClick: this.handleDateClick.bind(this), // bind is important!
-    events: [
-      { title: 'event 1', date: '2020-06-27' },
-      { title: 'event 2', date: '2020-06-30' }
-    ]
+    initialDate: '2020-06-07',
+   initialEvents: this.initialEvents, // alternatively, use the `events` setting to fetch from a feed
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    eventColor: '#378006'
+
+    // select: this.handleDateSelect.bind(this),
+    // eventClick: this.handleEventClick.bind(this),
+    // eventsSet: this.handleEvents.bind(this)
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
   };
-  handleDateClick(arg:any) {
-    alert('date click! ' + arg.dateStr)
-  }
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
-
