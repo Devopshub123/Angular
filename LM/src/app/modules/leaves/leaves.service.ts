49ab8e267f67775ad44a3f4e4f57e1471e658439
bknasam <bknasam@sreebtech.com>
2022-07-27 17:47:14 +0530

    updated compoff functionality

--- LM/src/app/modules/leaves/leaves.service.ts
+++ LM/src/app/modules/leaves/leaves.service.ts
@@ -75,4 +75,29 @@ cancelLeaveRequest(info:any): Observable<any> {
   return this.http.post(this.mainUrl+'api/cancelLeaveRequest',JSON.stringify(info), this.httpOptions);
 
 }
+
+setCompOff(info:any): Observable<any> {
+  return this.http.post(this.mainUrl+'api/setCompOff', JSON.stringify(info), this.httpOptions);
+}
+getCompOff(employeeId:any,rmid:any): Observable<any>{
+  return this.http.get(this.mainUrl+'api/getCompOff/'+employeeId+'/'+rmid, this.httpOptions);
+}
+
+getCompOffMinWorkingHours(): Observable<any>{
+  return this.http.get(this.mainUrl+'api/getCompOffMinWorkingHours', this.httpOptions);
+
+}
+getCompoffCalender(info:any): Observable<any>{
+  console.log(info,'info')
+  return this.http.get(this.mainUrl+'api/getCompoffCalender/'+JSON.stringify(info), this.httpOptions);
+}
+setCompOffReviewApprove(info:any): Observable<any> {
+  return this.http.post(this.mainUrl+'api/setCompOffReviewApprove', JSON.stringify(info), this.httpOptions);
+}
+
+getDurationforBackdatedCompoffLeave(info:any): Observable<any>{
+  console.log(info,'info3333')
+  return this.http.get(this.mainUrl+'api/getDurationforBackdatedCompoffLeave', this.httpOptions);
+
+}
 }
--- LM/src/app/modules/leaves/pages/user-compoff/user-compoff.component.ts
+++ LM/src/app/modules/leaves/pages/user-compoff/user-compoff.component.ts
@@ -1,30 +1,180 @@
-import { Component, OnInit } from '@angular/core';
+import { LeavesService } from './../../leaves.service';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { FormGroup,FormControl,Validators, FormBuilder, AbstractControl, FormArray} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import {MatDialog} from "@angular/material/dialog";
+import { DatePipe} from '@angular/common';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { ReusableDialogComponent } from 'src/app/pages/reusable-dialog/reusable-dialog.component';
+// import * as _moment from 'moment';
+// import { Moment } from 'moment';
+
+// const moment = _moment;
 @Component({
   selector: 'app-user-compoff',
   templateUrl: './user-compoff.component.html',
   styleUrls: ['./user-compoff.component.scss']
 })
 export class UserCompoffComponent implements OnInit {
+  max:any ;
+  min:any;
   CompoffForm:any= FormGroup;
-  private exportTime = { hour: 7, minute: 15, meriden: 'PM', format: 24 };
+  compOffDates:any=[];
+  compOffDateshide:any=[];
 
-  constructor(private formBuilder: FormBuilder,private router: Router,) { }
+  minimumHours:any=[];
+  usersession:any=[];
+  calender:any={};
+  compOffDetails:any=[];
+  year:any;
+  month:any;
+ 
+  today:any=new Date();
+  ishide:boolean=true;
+  displayedColumns: string[] = ['appliedon','workeddate','hours','status','approver'];
+  dataSource: MatTableDataSource<any>=<any>[];
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort!: MatSort;
+  constructor(private formBuilder: FormBuilder,private router: Router,private LM:LeavesService,public datepipe: DatePipe,public dialog: MatDialog) { 
+    this.usersession = JSON.parse(sessionStorage.getItem('user') || '')
+    this.year = this.today.getFullYear();
+    this.month = this.today.getMonth();
+    this.max = (new Date());
+    // this.CompoffForm.empId.controls.setValue(this.usersession.id);
+    // // this.compOff.empRollnumber=this.usersession.empid;
+    // this.CompoffForm.empName.controls.setValue(this.usersession.firstname+' '+ this.usersession.lastname);
+
+  }
 
   ngOnInit(): void {
+    this.getCompOffMinWorkingHours();
+    this.getCompOff();
+    this.getCompoffCalender();
+    this.getDurationforBackdatedCompoffLeave();
     this.CompoffForm=this.formBuilder.group(
       {
-      empid: ["",Validators.required],        
-      empname: ["",],
+      empId: ["",Validators.required],        
+      empName: ["",],
       workeddate:[""],
-      workedtime:["",],
+      hours:["",],
+      minutes:["",],
       reason:["",]
       
     });
+    this.CompoffForm.get('hours')?.valueChanges.subscribe((selectedValue:any) => {
+      if(selectedValue == 24){
+       this.ishide = false;
+      }
+      else{
+        this.ishide = true;
+      }
+    })
   }
-  submit(){}
-  cancel(){}
+
+  getCompOffMinWorkingHours(){
+    this.LM.getCompOffMinWorkingHours().subscribe((result)=> {
+      if(result.status){
+        for(let i=result.data[0].value;i<=24;i++){
+          this.minimumHours.push({value:i})
+        }
+      }
+    })
+
+  }
+  // getCompOff(){}
+  getCompOff(){
+    this.LM.getCompOff(this.usersession.id,this.usersession.rmid).subscribe((result)=> {
+      console.log(result);
+      if(result.status){
+        this.compOffDetails = result.data;
+        console.log(result.data)
+        this.dataSource = new MatTableDataSource(this.compOffDetails);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+    })
+
+
+  }
+  // getCompoffCalender(){}
+  getDurationforBackdatedCompoffLeave(){
+    this.LM.getDurationforBackdatedCompoffLeave(this.calender).subscribe((result)=> {
+      if(result.status){
+        this.min = (new Date(this.today.setDate(this.today.getDate()-result.data[0].value)));
+      }
+    });
+  
+  }
+  
+  getCompoffCalender(){
+    this.calender.employeeId=this.usersession.id;
+    this.calender.year=this.year.toString()
+    // this.calender.month='0'+(this.month+1).toString();
+    // this.calender.month.length === 3?this.calender.month.substring(1):this.calender.month;
+    this.LM.getCompoffCalender(this.calender).subscribe((result)=> {
+      if(result.status) {
+        // new Date(this.today.setDate(this.today.getDate()-result.data[0].value)
+        console.log('result',result)
+        for(let i= 0;i<result.data.length;i++){
+          this.compOffDates.push(new Date(result.data[i].edate))
+        }
+        console.log(' this.compOffDates', this.compOffDates)
+       
+      //   this.compOffDateshide = (d: Date): boolean => {
+      //     const date = d || new Date() ;
+         
+      //     return !this.compOffDates;
+      // }
+      }
+
+    })
+
+
+  }
+  
+  submit(){
+    let data={
+      rmId:this.usersession.roles[0].rmid,
+      id:null,
+      remarks:null,
+      status:'Submitted',
+      empRollnumber:this.usersession.id,
+      empName:this.usersession.firstname+' '+ this.usersession.lastname,
+      empId:this.usersession.id,
+      workedMinutes:this.CompoffForm.controls.minutes.value?this.CompoffForm.controls.minutes.value:0,
+      workDate:this.datepipe.transform(this.CompoffForm.controls.workeddate.value, 'yyyy-MM-dd'),
+      reason:this.CompoffForm.controls.reason.value,
+      workedHours:this.CompoffForm.controls.hours.value
+    }
+    this.LM.setCompOff(data).subscribe((result)=> {
+      if(result.status){
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() =>
+        this.router.navigate(["/LeaveManagement/UserCompOff"]));
+        let dialogRef = this.dialog.open(ReusableDialogComponent, {
+          position:{top:`70px`},
+          disableClose: true,
+          data: 'Compoff registerd successfully.'
+        });  
+        
+      }else{
+        let dialogRef = this.dialog.open(ReusableDialogComponent, {
+          position:{top:`70px`},
+          disableClose: true,
+          data: 'Unable to register compoff.'
+        });
+      }
+      
+    })
+
+  }
+  cancel(){
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() =>
+        this.router.navigate(["/LeaveManagement/UserCompOff"]));
+  }
   onChangeHour(event:Event){}
 
 }
