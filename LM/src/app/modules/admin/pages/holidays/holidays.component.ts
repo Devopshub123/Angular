0199167d74d72fa20e5a5eef1f815fabe66f1466
rthallapelly <rthallapelly@sreebtech.com>
2022-08-05 18:32:32 +0530

    updated userdashboard functionality and holidays issue fixed

--- LM/src/app/modules/admin/pages/holidays/holidays.component.ts
+++ LM/src/app/modules/admin/pages/holidays/holidays.component.ts
@@ -130,9 +130,9 @@ export class HolidaysComponent implements OnInit {
   add(){
     this.isview = false;
     this.isadd = true;
-    this.HolidayForm.controls.holiday.setValue('')
-    this.HolidayForm.controls.branch.setValue('')
-    this.HolidayForm.controls.date.setValue('')
+    this.HolidayForm.controls.holiday.setValue()
+    this.HolidayForm.controls.branch.value
+    this.HolidayForm.controls.holiday.value
 
   }
   /**Search functionality */
