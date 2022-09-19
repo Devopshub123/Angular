0f84c7bfcfe07c2bdd16f0db2bdc75c9e4bafcd5
sreeb <you@example.com>
2022-09-19 17:43:21 +0530

    Updated error message for reason

--- LM/src/app/modules/attendance/dialog/dialog.component.ts
+++ LM/src/app/modules/attendance/dialog/dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AdminService } from 'src/app/modules/admin/admin.service';
 
 @Component({
   selector: 'app-dialog',
@@ -9,15 +10,19 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class DialogComponent implements OnInit {
     public form:FormGroup;
- 
+  messagesDataList:any;
+  ATT1:any;
+
   constructor(private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<DialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
-      this.form = this.formBuilder.group({ 
+    @Inject(MAT_DIALOG_DATA) public data: any,private Admin:AdminService) {
+      this.form = this.formBuilder.group({
         'reason':['',Validators.maxLength(250)], });
     }
 rejectreason='';
   ngOnInit(): void {
+    this.getMessagesList();
+
     // this.requestform = this.formBuilder.group(
     //   {
     //     reason: ['',[Validators.required]],
@@ -37,12 +42,36 @@ rejectreason='';
         if(this.form.valid && this.form.value){
           this.dialogRef.close(this.form.value);
         }
-      
+
   }else{
     this.form.get('reason')?.clearValidators();
     this.form.get('reason')!.updateValueAndValidity();
-   
+
     this.dialogRef.close(this.form.value);
   }
+  }
+
+
+  getMessagesList() {
+    let data =
+      {
+        "code": null,
+        "pagenumber":1,
+        "pagesize":100
+      }
+    this.Admin.getMessagesListApi(data).subscribe((res:any)=>{
+      if(res.status) {
+        this.messagesDataList = res.data;
+        this.messagesDataList.forEach((e: any) => {
+          if (e.code == "ATT1") {
+            this.ATT1 = e.message
+          }
+        })
+      }
+      else {
+        this.messagesDataList = [];
+      }
+
+    })
   }
 }
--- LM/src/app/modules/leaves/dialog/review-and-approvals/review-and-approvals.component.ts
+++ LM/src/app/modules/leaves/dialog/review-and-approvals/review-and-approvals.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {DialogComponent} from "../../../attendance/dialog/dialog.component";
+import {LeavesService} from '../../leaves.service'
 @Component({
   selector: 'app-review-and-approvals',
   templateUrl: './review-and-approvals.component.html',
@@ -9,11 +10,13 @@ import {DialogComponent} from "../../../attendance/dialog/dialog.component";
 })
 export class ReviewAndApprovalsComponent implements OnInit {
    form:any=FormGroup;
+    LM1:any;
 
 
-  constructor(private formBuilder: FormBuilder,public dialogRef: MatDialogRef<DialogComponent>,@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(private formBuilder: FormBuilder,public dialogRef: MatDialogRef<DialogComponent>,@Inject(MAT_DIALOG_DATA) public data: any,private LM:LeavesService) { }
 
   ngOnInit(): void {
+    this.getErrorMessages('LM1');
     this.form = this.formBuilder.group({
       'reason':['',Validators.required], });
   }
@@ -29,15 +32,25 @@ export class ReviewAndApprovalsComponent implements OnInit {
         if(this.form.valid && this.form.value){
           this.dialogRef.close(this.form.value);
         }
-  
+
       }else{
         this.form.get('reason')?.clearValidators();
         this.form.get('reason')!.updateValueAndValidity();
-  
+
         this.dialogRef.close(this.form.value);
       }
 
     }
-   
+
+  }
+
+  getErrorMessages(errorCode:any) {
+    this.LM.getErrorMessages(errorCode,1,1).subscribe((result)=>{
+      if(result.status && errorCode == 'LM1')
+      {
+        this.LM1 = result.data[0].errormessage
+      }
+
+    })
   }
 }
