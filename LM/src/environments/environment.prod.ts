2e17256371e39c2c5135b88be41ed67ad546d3c1
rthallapelly <rthallapelly@sreebtech.com>
2023-01-17 14:03:58 +0530

    issues fixed

--- LM/src/environments/environment.prod.ts
+++ LM/src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
   /** static url */
-  apiUrl: 'http://122.175.62.210:6464/',
+  // apiUrl: 'http://122.175.62.210:6464/',
+  apiUrl:'http://122.175.62.210:6464/',
   /** local build */
   // apiUrl:'http://192.168.1.5:6464/',
-  dbName:'spryple_client'
+  dbName:'spryple_qa'
 };
