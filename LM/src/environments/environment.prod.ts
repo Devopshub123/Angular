feacbed1027aefb326eab44b6b965842a36baf86
maheshchennagani <mchennagani@sreebtech.com>
2023-02-27 10:42:26 +0530

    worked on QA build release

--- LM/src/environments/environment.prod.ts
+++ LM/src/environments/environment.prod.ts
@@ -1,13 +1,15 @@
+
 export const environment = {
   production: true,
-  /** static url */
-  //spryple AWS
-  apiUrl: 'http://13.232.185.196:6060/', 
+
+ /** spryple AWS */
+  // apiUrl: 'http://13.232.185.196:6060/', 
   
-  //payroll old
+ /** static url */
   // apiUrl: 'http://122.175.62.210:202/',
   
   /** local build */
-  // apiUrl:'http://192.168.1.20:202/',
+  apiUrl: 'http://192.168.1.20:202/',
+  
   dbName:sessionStorage.getItem('companyName'),
 };
