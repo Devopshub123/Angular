683559c0aa899953101f323f6788f68cc6053525
bknasam <bknasam@sreebtech.com>
2023-05-03 12:27:08 +0530

    update the environmen

--- LM/src/environments/environment.ts
+++ LM/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl:'http://localhost:6060/', //dev Env
-  // apiUrl: 'http://192.168.1.45:202/',
+  // apiUrl: 'http://192.168.1.10:7676/',
   dbName: sessionStorage.getItem('companyName'),
  //  dbName:'spryple'
 };
