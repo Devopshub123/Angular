c4767872690cfd50e7a0f72006f170ff57432c1d
rakesh <rthallapelly@sreebtech.com>
2023-05-15 11:37:56 +0530

    subscription module issues fixed

--- LM/src/app/modules/admin/subscription/register-validation/register-validation.component.ts
+++ LM/src/app/modules/admin/subscription/register-validation/register-validation.component.ts
@@ -37,81 +37,65 @@ export class RegisterValidationComponent implements OnInit {
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       'companyname':['',[Validators.minLength(5), Validators.required]],
-      'username': ['', [Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),Validators.required]],
+      'username': ['', [Validators.email, Validators.pattern('^[a-zA-Z0-9.-]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}'),Validators.required]],
       // 'recaptcha': ['', Validators.required],
       
     });
   }
   submit(){
     this.issubmit = true;
-   if( this.captchavalid &&  this.formGroup.controls.companyname.value !='' && this.formGroup.controls.username.value !=''){
-     let data = {
-      company_name_value:null,
-      company_code_value:this.formGroup.controls.companyname.value,
-      company_size_value:null,
-      number_of_users_value:null, 
-      plan_id_value:null,
-      industry_type_pm:null, 
-      industry_type_value_pm:null,
-      mobile_number_value: null,
-      company_email_value:this.formGroup.controls.username.value,
-      contact_name_value:null,
-      company_address1_value:null,
-      company_address2_value:null,
-      country_id_value:null,
-      state_id_value:null,
-      city_id_value:null,
-      pincode_value:null,
-      gst_number_value:null,
-      agree_to_terms_and_conditions_value: null,
-      steps_completed_value:1,
-      id_value:null,
-      created_by_value:null,
-        // email :this.formGroup.controls.username.value,
-        // companycode:this.formGroup.controls.companyname.value,
-        // company_name_value:null,
-        // company_size_value:null,
-        // plan_id_value:null,
-        // number_of_users_value:null,
-        // industry_type_pm:null,
-        // industry_type_value_pm:null,
-        // mobile_number_value:null,
-        // company_address1_value:null,
-        // company_address2_value:null,
-        // country_id_value:null,
-        // state_id_value:null,
-        // city_id_value:null,
-        // pincode_value:null,
-        // gst_number_value:null,
-        // agree_to_terms_and_conditions_value:null,
-        // steps_completed_value:1,
-        // id_value:null,
-        // created_by_value:null,
-        // contact_name_value:null
-  
-      }
-     this.AS.Validateemail(data).subscribe((result:any)=>{
-       if (result.status) {
-         this.isVerified = true;
-        // let dialogRef = this.dialog.open(ReusableDialogComponent, {
-        //   position:{top:`70px`},
-        //   disableClose: true,
-        //   data:result.message
-        // });
-        // this.router.navigateByUrl('/', { skipLocationChange: true }).then(() =>
-        //       this.router.navigate(["/Validateemail"]));
-      }
-      else{
-        let dialogRef = this.dialog.open(ReusableDialogComponent, {
-          position:{top:`70px`},
-          disableClose: true,
-          data:result.message
-        });
-         this.formGroup.controls.companyname.reset();
-      }
-    })
+    if(this.formGroup.valid){
+      if( this.captchavalid &&  this.formGroup.controls.companyname.value !='' && this.formGroup.controls.username.value !=''){
+        let data = {
+         company_name_value:null,
+         company_code_value:this.formGroup.controls.companyname.value,
+         company_size_value:null,
+         number_of_users_value:null, 
+         plan_id_value:null,
+         industry_type_pm:null, 
+         industry_type_value_pm:null,
+         mobile_number_value: null,
+         company_email_value:this.formGroup.controls.username.value,
+         contact_name_value:null,
+         company_address1_value:null,
+         company_address2_value:null,
+         country_id_value:null,
+         state_id_value:null,
+         city_id_value:null,
+         pincode_value:null,
+         gst_number_value:null,
+         agree_to_terms_and_conditions_value: null,
+         steps_completed_value:1,
+         id_value:null,
+         created_by_value:null,
+    
+         }
+        this.AS.Validateemail(data).subscribe((result:any)=>{
+          if (result.status) {
+            this.isVerified = true;
+           // let dialogRef = this.dialog.open(ReusableDialogComponent, {
+           //   position:{top:`70px`},
+           //   disableClose: true,
+           //   data:result.message
+           // });
+           // this.router.navigateByUrl('/', { skipLocationChange: true }).then(() =>
+           //       this.router.navigate(["/Validateemail"]));
+         }
+         else{
+           let dialogRef = this.dialog.open(ReusableDialogComponent, {
+             position:{top:`70px`},
+             disableClose: true,
+             data:result.message
+           });
+            this.formGroup.controls.companyname.reset();
+         }
+       })
+   
+       }
+    }else{
 
     }
+
     
     
   }
