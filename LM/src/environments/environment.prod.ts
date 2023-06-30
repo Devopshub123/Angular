4054cc490fc95339567dfb8d271aad6e47b15800
maheshchennagani <mchennagani@sreebtech.com>
2023-06-30 15:45:44 +0530

    worked on prod build

--- LM/src/environments/environment.prod.ts
+++ LM/src/environments/environment.prod.ts
@@ -2,18 +2,13 @@
 export const environment = {
   production: true,
 
- /** spryple AWS */
-  // apiUrl: 'http://13.232.185.196:6060/',
 
  /** static QA-202 & demo -2121- URL*/
   // apiUrl: 'http://122.175.62.210:7676/',
 
-  /** local build */
-  // apiUrl: 'http://192.168.0.50:7676/',
-
   /**-------------------------------------------------- */
   /**offline build */
-  apiUrl:'http://192.168.1.2:6060/',
+  apiUrl:'http://192.168.0.14:6060/',
 
   dbName:sessionStorage.getItem('companyName'),
 };
