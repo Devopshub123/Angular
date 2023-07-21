d1e77a9904707618bd4d770dcc1f8c3fb6a29776
maheshchennagani <mchennagani@sreebtech.com>
2023-07-21 14:36:28 +0530

    ip configuration fixed

--- LM/src/environments/environment.prod.ts
+++ LM/src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
 
   /**-------------------------------------------------- */
   /**offline build */
-  apiUrl:'http://localhost:6060/',
+  apiUrl:'http://192.168.1.122:6060/',
 
   dbName:sessionStorage.getItem('companyName'),
 };
