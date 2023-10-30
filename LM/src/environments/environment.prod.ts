e4a3bc035bd66dc23d8e39efa42d104df9f0fae8
Devopshub123 <128797715+Devopshub123@users.noreply.github.com>
2023-10-30 16:26:41 +0530

    Update environment.prod.ts

--- LM/src/environments/environment.prod.ts
+++ LM/src/environments/environment.prod.ts
@@ -1,14 +1,3 @@
-
 export const environment = {
-  production: true,
-
-
- /** static QA-202 & demo -2121- URL*/
-  // apiUrl: 'http://122.175.62.210:7676/',
-
-  /**-------------------------------------------------- */
-  /**offline build */
-  apiUrl:'http://35.91.144.240:6060/',
-
-  dbName:sessionStorage.getItem('companyName'),
+  production: true
 };
